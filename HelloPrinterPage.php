bd58ff34f67cf5f6fa96adeebd7b11a5b333d773
ToSzi <t.horvath@rgu.ac.uk>
2017-03-08 18:44:29 +0000

    Initial commit

---
+++ HelloPrinterPage.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ToSzi
+ * Date: 3/8/2017
+ * Time: 6:37 PM
+ */
+
+$planet2Display = $_GET[$planet];
+
+echo"Hello {$planet2Display}";
---
+++ Index.php
@@ -0,0 +1,29 @@
+/**
+ * Created by PhpStorm.
+ * User: ToSzi
+ * Date: 3/8/2017
+ * Time: 6:30 PM
+ */
+
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <title>Hello World</title>
+        <meta charset="UTF-8">
+    </head>
+</body>
+    <header>
+        <h1>This is another Hello world!!!!</h1>
+    </header>
+    <main>
+            <fieldset>
+                <legend>Say Hello</legend>
+                <ul>
+                    <li><a href="HelloPrinterPage.php?planet=earth">Earth</a></li><br />
+                    <li><a href="HelloPrinterPage.php?planet=mars">Mars</a></li><br />
+                    <li><a href="HelloPrinterPage.php?planet=Uranus">Uranus</a></li>
+                </ul>
+            </fieldset>
+    </main>
+</body>
+</html>
